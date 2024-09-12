0d8d5370011b2bfcd977c706b5437e1fb1f48720
unknown <sogoal17@naver.com>
2024-09-12 10:30:30 -0500

    second commit

---
+++ block_7/index.js
@@ -0,0 +1,15 @@
+
+// Pseudocode: lock 1 : 15-5 = 10
+let lockNum1 =15
+lockNum1 = lockNum1 - 5
+// Pseudocode: lock 2 : 80/40 = 40
+let lockNum2 = 80
+lockNum2 = lockNum2 / 2
+// Pseudocode: lock 3 : 30+9 = 39
+let lockNum3 = 30
+lockNum3 = lockNum3 + 9
+
+//Pseudocode: Create alert box including lockcode
+function show_alert(){
+    alert(`The combination of the lock is ${lockNum1} - ${lockNum2} - ${lockNum3}`)
+}
